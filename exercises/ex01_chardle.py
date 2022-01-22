8c67df14f8e159a944338174b7937dd0ab1f6f25
Zack Bennett <benzac12@live.unc.edu>
2022-01-22 11:58:04 -0500

    commit

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,9 +2,9 @@
 
 __author__ = "730232406"
 
-guess: str = str(input("Enter a 5-character word: "))
+choice: str = str(input("Enter a 5-character word: "))
 
-if (len(Guess)) != int(5):
+if (len(choice)) != int(5):
     print("Error: Word must contain 5 characters")
     exit()
 
@@ -14,31 +14,31 @@ if (len(letter)) != int(1):
     print("Error: Character must be a single character.")
     exit()
 
-print("Searching for " + letter + " in " + guess)
+print("Searching for " + letter + " in " + choice)
 
 x: int = 0
 
-if letter == guess[0]:
+if letter == choice[0]:
     print(letter + " found at index 0")
     x = x + 1
-if letter == guess[1]:
+if letter == choice[1]:
     print(letter + " found at index 1")
     x = x + 1
-if letter == guess[2]:
+if letter == choice[2]:
     print(letter + " found at index 2")
     x = x + 1
-if letter == guess[3]:
+if letter == choice[3]:
     print(letter + " found at index 3")
     x = x + 1
-if letter == guess[4]:
+if letter == choice[4]:
     print(letter + " found at index 4")
     x = x + 1
 
 if x == 0:
-    print("No instances of " + letter + " found in " + guess)
+    print("No instances of " + letter + " found in " + choice)
 else:
     if x == 1:
-        print(str(x) + " instance of " + letter + " found in " + guess)
+        print(str(x) + " instance of " + letter + " found in " + choice)
     else:
-        print(str(x) + " instances of " + letter + " found in " + guess)
+        print(str(x) + " instances of " + letter + " found in " + choice)
    
